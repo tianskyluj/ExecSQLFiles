06407c3f2e7604aa45e9539a0dab4b6dfb04fb65
唐富伟 <tianskyluj@sina.com>
2013-06-26 17:43:49 +0800

    初始化程序

---
+++ Program/batchSql.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace batchSql
+{
+    partial class batchSql
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.serverUrl_eidt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.database_edit = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.uid_edit = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pwd_edit = new System.Windows.Forms.TextBox();
+            this.testConnect = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.fileSelection = new System.Windows.Forms.Button();
+            this.execSql = new System.Windows.Forms.Button();
+            this.fileUrl_show = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.result_ok = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.result_faild = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "服务器地址：";
+            // 
+            // serverUrl_eidt
+            // 
+            this.serverUrl_eidt.Location = new System.Drawing.Point(96, 16);
+            this.serverUrl_eidt.Name = "serverUrl_eidt";
+            this.serverUrl_eidt.Size = new System.Drawing.Size(176, 21);
+            this.serverUrl_eidt.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "数据库名称:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // database_edit
+            // 
+            this.database_edit.Location = new System.Drawing.Point(96, 65);
+            this.database_edit.Name = "database_edit";
+            this.database_edit.Size = new System.Drawing.Size(176, 21);
+            this.database_edit.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "用户名:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // uid_edit
+            // 
+            this.uid_edit.Location = new System.Drawing.Point(96, 106);
+            this.uid_edit.Name = "uid_edit";
+            this.uid_edit.Size = new System.Drawing.Size(176, 21);
+            this.uid_edit.TabIndex = 5;
+            this.uid_edit.Text = "sa";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "密码:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pwd_edit
+            // 
+            this.pwd_edit.Location = new System.Drawing.Point(96, 151);
+            this.pwd_edit.Name = "pwd_edit";
+            this.pwd_edit.Size = new System.Drawing.Size(176, 21);
+            this.pwd_edit.TabIndex = 7;
+            // 
+            // testConnect
+            // 
+            this.testConnect.Location = new System.Drawing.Point(193, 189);
+            this.testConnect.Name = "testConnect";
+            this.testConnect.Size = new System.Drawing.Size(79, 25);
+            this.testConnect.TabIndex = 8;
+            this.testConnect.Text = "测试连接";
+            this.testConnect.UseVisualStyleBackColor = true;
+            this.testConnect.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(24, 198);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(84, 16);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "无密码登录";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // fileSelection
+            // 
+            this.fileSelection.Location = new System.Drawing.Point(337, 65);
+            this.fileSelection.Name = "fileSelection";
+            this.fileSelection.Size = new System.Drawing.Size(148, 149);
+            this.fileSelection.TabIndex = 10;
+            this.fileSelection.Text = "选择文件";
+            this.fileSelection.UseVisualStyleBackColor = true;
+            this.fileSelection.Click += new System.EventHandler(this.fileSelection_Click);
+            // 
+            // execSql
+            // 
+            this.execSql.Location = new System.Drawing.Point(548, 65);
+            this.execSql.Name = "execSql";
+            this.execSql.Size = new System.Drawing.Size(148, 149);
+            this.execSql.TabIndex = 11;
+            this.execSql.Text = "执行sql文件";
+            this.execSql.UseVisualStyleBackColor = true;
+            this.execSql.Click += new System.EventHandler(this.execSql_Click);
+            // 
+            // fileUrl_show
+            // 
+            this.fileUrl_show.AutoSize = true;
+            this.fileUrl_show.Location = new System.Drawing.Point(335, 19);
+            this.fileUrl_show.Name = "fileUrl_show";
+            this.fileUrl_show.Size = new System.Drawing.Size(0, 12);
+            this.fileUrl_show.TabIndex = 12;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(24, 238);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(782, 246);
+            this.tabControl1.TabIndex = 13;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.result_ok);
+            this.tabPage1.Location = new System.Drawing.Point(4, 21);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(774, 221);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "执行成功";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // result_ok
+            // 
+            this.result_ok.Location = new System.Drawing.Point(7, 7);
+            this.result_ok.MaximumSize = new System.Drawing.Size(767, 210);
+            this.result_ok.Multiline = true;
+            this.result_ok.Name = "result_ok";
+            this.result_ok.Size = new System.Drawing.Size(767, 210);
+            this.result_ok.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.result_faild);
+            this.tabPage2.Location = new System.Drawing.Point(4, 21);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(774, 221);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "执行失败";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // result_faild
+            // 
+            this.result_faild.Location = new System.Drawing.Point(4, 5);
+            this.result_faild.MaximumSize = new System.Drawing.Size(767, 210);
+            this.result_faild.Multiline = true;
+            this.result_faild.Name = "result_faild";
+            this.result_faild.Size = new System.Drawing.Size(767, 210);
+            this.result_faild.TabIndex = 1;
+            // 
+            // batchSql
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(818, 496);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.fileUrl_show);
+            this.Controls.Add(this.execSql);
+            this.Controls.Add(this.fileSelection);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.testConnect);
+            this.Controls.Add(this.pwd_edit);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.uid_edit);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.database_edit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.serverUrl_eidt);
+            this.Controls.Add(this.label1);
+            this.Name = "batchSql";
+            this.Text = "批量执行sql文件";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox serverUrl_eidt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox database_edit;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox uid_edit;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox pwd_edit;
+        private System.Windows.Forms.Button testConnect;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button fileSelection;
+        private System.Windows.Forms.Button execSql;
+        private System.Windows.Forms.Label fileUrl_show;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox result_ok;
+        private System.Windows.Forms.TextBox result_faild;
+    }
+}
+
---
+++ Program/batchSql.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+using System.Collections;
+
+namespace batchSql
+{
+    public partial class batchSql : Form
+    {
+        public batchSql()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.checkBox1.Checked)
+            {
+                this.uid_edit.Enabled = false;
+                this.pwd_edit.Enabled = false;
+            }
+            else
+            {
+                this.uid_edit.Enabled = true;
+                this.pwd_edit.Enabled = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.serverUrl_eidt.Text.Trim() == "")
+            {
+                MessageBox.Show("请输入服务器地址");
+                return;
+            }
+            if (this.database_edit.Text.Trim() == "")
+            {
+                MessageBox.Show("请输入数据库名称");
+                return;
+            }
+               
+
+            bool IsCanConnectioned = false;
+
+            SqlConnection objConnection=new SqlConnection(this.getConnectString());
+
+
+            try
+            {
+                //Open DataBase
+                //打开数据库
+                objConnection.Open();
+                IsCanConnectioned = true;
+            }
+            catch
+            {
+                //Can not Open DataBase
+                //打开不成功 则连接不成功
+                IsCanConnectioned = false;
+            }
+            finally
+            {
+                //Close DataBase
+                //关闭数据库连接
+                objConnection.Close();
+            }
+            objConnection.Close();
+            if (IsCanConnectioned)
+                MessageBox.Show("链接成功");
+            else
+                MessageBox.Show("链接失败，请检查输入字符串是否正确");
+        }
+
+        protected string getConnectString()
+        {
+            string strConnection = string.Empty;
+            if (this.checkBox1.Checked)
+            {
+                strConnection = "integrated security=true;";
+                strConnection += "initial catalog=" + this.database_edit.Text.Trim() + ";Server=" + this.serverUrl_eidt.Text.Trim() + ";";
+                strConnection += "Connect Timeout=30";
+            }
+            else
+            {
+                strConnection = "uid="+this.uid_edit.Text.Trim()+";password="+this.pwd_edit.Text.Trim()+";";
+                strConnection += "initial catalog="+this.database_edit.Text.Trim()+";Server="+this.serverUrl_eidt.Text.Trim()+";";
+                strConnection += "Connect Timeout=5";
+            }
+            return strConnection;
+        }
+
+        private void fileSelection_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fd = new FolderBrowserDialog();
+            if (fd.ShowDialog() == DialogResult.OK)
+                fileUrl_show.Text = fd.SelectedPath;
+            //this.folderBrowserDialog1.ShowDialog();
+        }
+
+        /// <summary>
+        /// 执行Sql文件
+        /// </summary>
+        /// <param name="varFileName"></param>
+        /// <returns></returns>
+        public  bool ExecuteSqlFile(string varFileName)
+        {
+            if (!File.Exists(varFileName))
+            {
+                return false;
+            }
+
+            StreamReader sr = File.OpenText(varFileName);
+
+            ArrayList alSql = new ArrayList();
+
+            string commandText = "";
+
+            string varLine = "";
+
+            while (sr.Peek() > -1)
+            {
+                varLine = sr.ReadLine();
+                if (varLine == "")
+                {
+                    continue;
+                }
+                if (varLine != "GO")
+                {
+                    commandText += varLine;
+                    commandText += "\r\n";
+                }
+                else
+                {
+                    alSql.Add(commandText);
+                    commandText = "";
+                }
+            }
+
+            sr.Close();
+
+            try
+            {
+                ExecuteCommand(alSql);
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// 执行
+        /// </summary>
+        /// <param name="varSqlList"></param>
+        public  void ExecuteCommand(ArrayList varSqlList)
+        {
+            SqlConnection objConnection = new SqlConnection(this.getConnectString());
+
+            objConnection.Open();
+            SqlTransaction varTrans = objConnection.BeginTransaction();
+
+            SqlCommand command = new SqlCommand();
+            command.Connection = objConnection;
+            command.Transaction = varTrans;
+
+            try
+            {
+                foreach (string varcommandText in varSqlList)
+                {
+                    command.CommandText = varcommandText;
+                    command.ExecuteNonQuery();
+                }
+                varTrans.Commit();
+            }
+            catch (Exception ex)
+            {
+                varTrans.Rollback();
+                throw ex;
+            }
+            finally
+            {
+                objConnection.Close();
+            }
+        }
+
+        /// <summary>
+        /// 执行所有的sql文件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void execSql_Click(object sender, EventArgs e)
+        {
+            int successNum = 0;
+            int faildNum = 0;
+            foreach (string name in Directory.GetFiles(this.fileUrl_show.Text.Trim()))
+            {
+                if (ExecuteSqlFile(name))
+                {
+                    this.result_ok.Text +=name+" 操作成功" + Environment.NewLine;
+                    successNum++;
+                }
+                else
+                {
+                    this.result_faild.Text += name + " 操作失败" + Environment.NewLine;
+                    faildNum++;
+                }
+            }
+            MessageBox.Show("总共执行(" + Directory.GetFiles(this.fileUrl_show.Text.Trim()).Length.ToString()+") 成功("+successNum.ToString()+") 失败("+faildNum.ToString()+")");
+        }
+
+    }
+}
